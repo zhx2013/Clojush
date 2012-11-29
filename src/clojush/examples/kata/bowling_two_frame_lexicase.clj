44b5ba5ae11089bc8b5c8d694324aea4143eeebf
Tom Helmuth <thelmuth@cs.umass.edu>
2012-11-29 11:27:19 -0500

    Added bowling-two-frame-lexicase, and changed parameters for two-frame trials.

--- src/clojush/examples/kata/bowling_two_frame_lexicase.clj
+++ src/clojush/examples/kata/bowling_two_frame_lexicase.clj
@@ -1,4 +1,4 @@
-(ns clojush.examples.kata.bowling-two-frame
+(ns clojush.examples.kata.bowling-two-frame-lexicase
   (:use [clojush.pushgp.pushgp]
         [clojush.examples.kata.bowling-helper]))
 
@@ -17,10 +17,9 @@
   :mutation-max-points 10
   :crossover-probability 0.8
   :simplification-probability 0.05
-  :tournament-size 6
-  :trivial-geography-radius 10
   :node-selection-method :size-tournament
   :node-selection-tournament-size 2
   :report-simplifications 0
   :final-report-simplifications 1000
+  :use-lexicase-selection true
   )
