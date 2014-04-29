10d99221a2fd9c1f5fc75cc9a2af6df1f66d4c42
H Zhan <hzcommit@sina.com>
2014-04-29 19:34:11 -0400

    增加混血繁殖、纯种竞赛繁殖、纯种语义繁殖三种选项

--- src/clojush/pushgp/breed.clj
+++ src/clojush/pushgp/breed.clj
@@ -11,8 +11,9 @@
 
 (defn breed
   "The new breed function will do
-  30% tourney-breeding
-  70% lexicase-breeding"
+  10% interracial-breeding
+  20% Tourney-breeding
+  70% Lexicase-breeding"
   [agt location rand-gen population
    {:keys [;; genetic operator probabilities
            mutation-probability crossover-probability simplification-probability
@@ -28,9 +29,11 @@
     :as argmap}]
     (random/with-rng rand-gen
       (let [i (lrand)]
-	(if (< n 0.3)
+	(if (< n 0.1)
+	  (breed-interracial agt location rand-gen population argmap)
+	  (if (< n 0.3)
 	  (breed-tourney agt location rand-gen population argmap)
-	  (breed-lexicase agt location rand-gen population argmap)))))
+	  (breed-lexicase agt location rand-gen population argmap))))))
 
 (defn breed-tourney
   "This breed function would select parents with Tourney selection"
@@ -123,6 +126,79 @@
   (random/with-rng rand-gen
     (let [n (lrand)
           parent (select-lexicase population location argmap)]
+      (assoc
+        (cond
+          ;; mutation
+          (< n mutation-probability)
+          (mutate parent argmap)
+          ;; crossover
+          (< n (+ mutation-probability crossover-probability))
+          (let [second-parent (select-lexicase population location argmap)]
+            (crossover parent second-parent argmap))
+          ;; simplification
+          (< n (+ mutation-probability crossover-probability simplification-probability))
+          (auto-simplify parent error-function reproduction-simplifications false 1000 maintain-ancestors)
+          ;; gaussian mutation
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability))
+          (gaussian-mutate parent argmap)
+          ;; boolean gsxover
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability boolean-gsxover-probability))
+          (let [second-parent (select-lexicase population location argmap)]
+            (boolean-gsxover parent second-parent argmap))
+          ;; deletion mutation
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability boolean-gsxover-probability deletion-mutation-probability))
+          (delete-mutate parent argmap)
+          ;; parentheses addition mutation
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability boolean-gsxover-probability
+                  deletion-mutation-probability parentheses-addition-mutation-probability))
+          (add-parentheses-mutate parent argmap)
+          ;; tagging mutation
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability boolean-gsxover-probability
+                  deletion-mutation-probability parentheses-addition-mutation-probability
+                  tagging-mutation-probability))
+          (tagging-mutate parent @global-tag-limit argmap)
+          ;; tag branch mutation
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability boolean-gsxover-probability
+                  deletion-mutation-probability parentheses-addition-mutation-probability
+                  tagging-mutation-probability tag-branch-mutation-probability))        
+          (tag-branch-insertion-mutate parent @global-tag-limit argmap)
+          ;; ultra
+          (< n (+ mutation-probability crossover-probability simplification-probability 
+                  gaussian-mutation-probability boolean-gsxover-probability
+                  deletion-mutation-probability parentheses-addition-mutation-probability
+                  tagging-mutation-probability tag-branch-mutation-probability
+                  ultra-probability))
+          (let [second-parent (select-lexicase population location argmap)]
+            (ultra parent second-parent argmap))
+          ;; replication
+          true
+          parent)
+        :parent parent))))
+        
+(defn breed-interracial
+  "This breed function will select parent 1 with tourney selection and parent 2 with lexicase selection"
+  [agt location rand-gen population
+   {:keys [;; genetic operator probabilities
+           mutation-probability crossover-probability simplification-probability
+           gaussian-mutation-probability boolean-gsxover-probability
+           deletion-mutation-probability parentheses-addition-mutation-probability
+           tagging-mutation-probability tag-branch-mutation-probability
+           ultra-probability
+           ;; Used by select
+           tournament-size trivial-geography-radius
+           ;; Used by simplification
+           error-function reproduction-simplifications maintain-ancestors
+           ]
+    :as argmap}]
+  (random/with-rng rand-gen
+    (let [n (lrand)
+          parent (select-tourney population location argmap)]
       (assoc
         (cond
           ;; mutation
